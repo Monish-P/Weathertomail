85aad15ddefdd1c8e0a8a5e8250d05bfd9812c31
Monish-P <121910311046@gitam.in>
2022-01-11 18:43:44 +0530

    commited

--- weatherapi/settings.py
+++ weatherapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jki+$aawp#j#-&_g!f1bz_!s5@@zlssnlgfm67fob4@o+$@ock'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['weathertomail.herokuapp.com']
 
