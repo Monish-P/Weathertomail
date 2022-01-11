f93b2b533ab10b347a7cdd3215d2fc7808411b66
Monish-P <121910311046@gitam.in>
2022-01-11 13:48:39 +0530

    commited

--- weatherapi/settings.py
+++ weatherapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jki+$aawp#j#-&_g!f1bz_!s5@@zlssnlgfm67fob4@o+$@ock
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weathertomail.herokuapp.com']
 
 
 # Application definition
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
