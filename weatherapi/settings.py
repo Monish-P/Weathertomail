a27cfeeb170a7de6e30d32681c9f5380fba9460d
Monish-P <121910311046@gitam.in>
2022-01-11 13:59:52 +0530

    commited

--- weatherapi/settings.py
+++ weatherapi/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
+CRISPY_TEMPLATE_PACK='bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
