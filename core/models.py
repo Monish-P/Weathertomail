f0b0f99aa61f53a87361a625fc5bf0e24dc765a1
Monish-P <121910311046@gitam.in>
2022-01-11 17:38:17 +0530

    commited

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     name = models.CharField(max_length=1000)
     email = models.CharField(max_length=1000)
     city = models.CharField(max_length=1000)
-    time = models.DateTimeField(default=datetime.datetime.now())
+    time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
